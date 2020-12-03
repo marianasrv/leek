4edab975a5fe07cac1e8fa1f3dd534184ccdfb21
marianasrv <mariana.saraiva@tecnico.ulisboa.pt>
2020-12-03 21:11:10 +0000

    add month choice view and filter views

--- lib/tips.dart
+++ lib/tips.dart
@@ -11,6 +11,12 @@ class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 18.0);
+  bool _typeHerbs = false;
+  bool _typeVegetable = false;
+  bool _typeFruit = false;
+  bool _isDisabled = true;
+  bool _isMostPopular = false;
+  bool _isTrending = false;
 
   @override
   Widget build(BuildContext context) {
@@ -111,20 +117,25 @@ class _RandomWordsState extends State<RandomWords> {
                         borderRadius: BorderRadius.circular(20),
                       ),
                       elevation: 6.0,
-                      child: Row(children: [
-                        Padding(
-                            padding: EdgeInsets.fromLTRB(12, 10, 0, 10),
-                            child: Text("Filters",
-                                style: TextStyle(
-                                    color: Color(0xFF1A633C), fontSize: 13))),
-                        Padding(
-                            padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
-                            child: Icon(
-                              Icons.filter_list_alt,
-                              color: Color(0xFF1A633C),
-                              size: 25,
-                            ))
-                      ]),
+                      child: GestureDetector(
+                          onTap: () {
+                            _showcontent();
+                          },
+                          child: Row(children: [
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(12, 10, 0, 10),
+                                child: Text("Filters",
+                                    style: TextStyle(
+                                        color: Color(0xFF1A633C),
+                                        fontSize: 13))),
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                                child: Icon(
+                                  Icons.filter_list_alt,
+                                  color: Color(0xFF1A633C),
+                                  size: 25,
+                                ))
+                          ])),
                     ),
                   )))
         ],
@@ -165,6 +176,264 @@ class _RandomWordsState extends State<RandomWords> {
               ),
             ])),
       ],
+    );
+  }
+
+  void _showcontent() {
+    showDialog(
+      context: context, barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        var width = MediaQuery.of(context).size.width;
+        return new StatefulBuilder(builder: (context, setState) {
+          return AlertDialog(
+            content: new Container(
+                height: 230.0,
+                width: width - 10,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 6,
+                            child: Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 40, 0),
+                                child: Text(
+                                  "Type of Crop",
+                                  style: TextStyle(
+                                      color: Color(0xFF1A633C),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                )),
+                          ),
+                          FlatButton(
+                            padding: EdgeInsets.fromLTRB(60, 0, 0, 0),
+                            child: Icon(
+                              Icons.close_rounded,
+                              size: 30,
+                              color: Color(0xFF1A633C),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            child: new RaisedButton(
+                                child: new Text('Herb',
+                                    style: TextStyle(
+                                        color: _typeHerbs
+                                            ? Colors.white
+                                            : Color(0xFF707070),
+                                        fontSize: 12)),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                        color: _typeHerbs
+                                            ? Color(0xFF3FAF73)
+                                            : Color(0xFF707070))),
+                                color: _typeHerbs
+                                    ? Color(0xFF3FAF73)
+                                    : Colors.white,
+                                onPressed: () => {
+                                      setState(() => _typeHerbs = !_typeHerbs),
+                                      if (!_isMostPopular &&
+                                          !_isTrending &&
+                                          !_typeFruit &&
+                                          !_typeHerbs &&
+                                          !_typeVegetable)
+                                        {_isDisabled = true}
+                                      else
+                                        {_isDisabled = false}
+                                    }),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            child: new RaisedButton(
+                                color: _typeVegetable
+                                    ? Color(0xFF3FAF73)
+                                    : Colors.white,
+                                child: new Text('Vegetable',
+                                    style: TextStyle(
+                                        color: _typeVegetable
+                                            ? Colors.white
+                                            : Color(0xFF707070),
+                                        fontSize: 12)),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                        color: _typeVegetable
+                                            ? Color(0xFF3FAF73)
+                                            : Color(0xFF707070))),
+                                onPressed: () => {
+                                      setState(() =>
+                                          _typeVegetable = !_typeVegetable),
+                                      if (!_isMostPopular &&
+                                          !_isTrending &&
+                                          !_typeFruit &&
+                                          !_typeHerbs &&
+                                          !_typeVegetable)
+                                        {_isDisabled = true}
+                                      else
+                                        {_isDisabled = false}
+                                    }),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            child: new RaisedButton(
+                                color: _typeFruit
+                                    ? Color(0xFF3FAF73)
+                                    : Colors.white,
+                                child: new Text('Fruit',
+                                    style: TextStyle(
+                                        color: _typeFruit
+                                            ? Colors.white
+                                            : Color(0xFF707070),
+                                        fontSize: 12)),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                        color: _typeFruit
+                                            ? Color(0xFF3FAF73)
+                                            : Color(0xFF707070))),
+                                onPressed: () => {
+                                      setState(() => _typeFruit = !_typeFruit),
+                                      if (!_isMostPopular & !_isTrending &&
+                                          !_typeFruit &&
+                                          !_typeHerbs &&
+                                          !_typeVegetable)
+                                        {_isDisabled = true}
+                                      else
+                                        {_isDisabled = false}
+                                    }),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.fromLTRB(0, 20, 40, 0),
+                              child: Text(
+                                "Popularity",
+                                style: TextStyle(
+                                    color: Color(0xFF1A633C),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              )),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 10, 15, 0),
+                              child: new RaisedButton(
+                                child: new Text('Most Popular',
+                                    style: TextStyle(
+                                        color: _isMostPopular
+                                            ? Colors.white
+                                            : Color(0xFF707070),
+                                        fontSize: 12)),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                        color: _isMostPopular
+                                            ? Color(0xFF3FAF73)
+                                            : Color(0xFF707070))),
+                                color: _isMostPopular
+                                    ? Color(0xFF3FAF73)
+                                    : Colors.white,
+                                onPressed: () => {
+                                  setState(() => {
+                                        _isMostPopular = !_isMostPopular,
+                                        if (_isTrending)
+                                          {
+                                            _isTrending = !_isTrending,
+                                          }
+                                      }),
+                                  if (!_isMostPopular &&
+                                      !_isTrending &&
+                                      !_typeFruit &&
+                                      !_typeHerbs &&
+                                      !_typeVegetable)
+                                    {_isDisabled = true}
+                                  else
+                                    {_isDisabled = false}
+                                },
+                              )),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                            child: new RaisedButton(
+                                color: _isTrending
+                                    ? Color(0xFF3FAF73)
+                                    : Colors.white,
+                                child: new Text('Trending',
+                                    style: TextStyle(
+                                        color: _isTrending
+                                            ? Colors.white
+                                            : Color(0xFF707070),
+                                        fontSize: 12)),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                        color: _isTrending
+                                            ? Color(0xFF3FAF73)
+                                            : Color(0xFF707070))),
+                                onPressed: () => {
+                                      setState(() => {
+                                            _isTrending = !_isTrending,
+                                            if (_isMostPopular)
+                                              {
+                                                _isMostPopular =
+                                                    !_isMostPopular,
+                                              }
+                                          }),
+                                      if (!_isMostPopular &&
+                                          !_isTrending &&
+                                          !_typeFruit &&
+                                          !_typeHerbs &&
+                                          !_typeVegetable)
+                                        {_isDisabled = true}
+                                      else
+                                        {_isDisabled = false}
+                                    }),
+                          ),
+                        ],
+                      )
+                    ])),
+            actions: [
+              new Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 10, 0),
+                  child: RaisedButton(
+                    child: Text('Apply',
+                        style: TextStyle(color: Colors.white, fontSize: 12)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(
+                            color: _isDisabled
+                                ? Color(0x803FAF73)
+                                : Color(0xFF3FAF73))),
+                    color: Color(0xFF3FAF73),
+                    disabledColor: Color(0x803FAF73),
+                    onPressed: _isDisabled
+                        ? null
+                        : () {
+                            Navigator.of(context).pop();
+                          },
+                  )),
+            ],
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          );
+        });
+      },
     );
   }
 }
